9f7559c8ad93871b1351cdd4abf99923adde6412
Duukkis <duukkis@hotmail.com>
2018-03-25 21:36:34 +0300

    new class to rule em all

--- verbicate.php
+++ verbicate.php
@@ -1,5 +1,368 @@
 <?php
 
+
+/**
+* conjugate verbs
+*/
+class Verbicate
+{
+
+  // some helpers if we want to conjugate same word (performance)
+  private $word = null;
+  public $sylls = null;
+  public $orig = null;
+  public $nbr_of_sylls = null;
+  
+  public $last_syllabus = null;
+  private $backVowelWord = null;
+
+  private $wovels = array("a", "e", "i", "o", "u", "y");
+  /**
+  * constructor
+  * @return void
+  */
+  public function __construct($word = null) {
+    $this->plop($word);
+  }
+
+  private function plop($word){
+    $this->isBackWovelWord($word);
+    $this->word = $word;
+    $this->syllabs($word);
+  }
+  
+  public function preesensMe($word) {
+    $this->plop($word);
+    $w = $this->orig;
+    $nos = $this->nbr_of_sylls;
+    // aak-kos-taa
+    // $secondlast = aakko(s)taa
+    // $secondfirst = aak(k)ostaa
+    // thirdlast = aa(k)kostaa
+    if($nos >= 3){
+      $thirdlast = mb_substr($this->sylls[$nos-3], -1);
+    } else {
+      $thirdlast = "";
+    }
+    $secondfirst = mb_substr($this->sylls[$nos-2], 0, 1);
+    $secondlast = mb_substr($this->sylls[$nos-2], -1);
+    
+    if($this->backVowelWord){
+      $a = "a";
+    } else {
+      $a = "ä";
+    }
+    switch($this->last_syllabus){
+      case "taa": // tää
+        if($secondlast == "t"){ // laittaa
+          $w[$nos-1] = $a."n";
+        } else if($secondlast == "h" || in_array($secondlast, $this->wovels)){ // johtaa
+          $w[$nos-1] = "d".$a."n";
+        } else if($secondlast == "n"){ // juontaa
+          $w[$nos-1] = "n".$a."n";
+        } else if($secondlast == "r"){ // avartaa
+          $w[$nos-1] = "r".$a."n";
+        } else if($secondlast == "l"){ // kiiltää
+          $w[$nos-1] = "l".$a."n";
+        } else {
+          $w[$nos-1] = "t".$a."n";
+        }
+        break;
+      case "da": // dä
+        if ($secondlast == "h") { // tehdä, nähdä
+          $w[$nos-2] = mb_substr($w[$nos-2],0,-1);
+          $w[$nos-1] = "en";
+        } else {
+          $w[$nos-1] = "n";
+        }
+        break;
+      case "la": // lä
+        if(in_array($thirdlast, $this->wovels) && $w[$nos-2] == "tel"){ // haukotella
+          $w[$nos-3] .= "t";
+        } else if ($thirdlast == "l" && $secondfirst == "l"){ // kiellellä
+          $w[$nos-2] = "t".mb_substr($w[$nos-2],1);
+        } else if ($thirdlast == "r" && $secondfirst == "r"){ // kiellellä
+          $w[$nos-2] = "t".mb_substr($w[$nos-2],1);
+        } else if ($thirdlast == "n" && $secondfirst == "n"){ // annella
+          $w[$nos-2] = "t".mb_substr($w[$nos-2],1);
+        } else if ($thirdlast == "h" && $secondfirst == "d"){ // hypähdellä
+          $w[$nos-2] = "t".mb_substr($w[$nos-2],1);
+        } else if(in_array($thirdlast, $this->wovels) && $secondfirst == "d"){ // huudella
+          $w[$nos-2] = "t".mb_substr($w[$nos-2],1);
+        }
+        if ($secondlast == "l") {
+          $w[$nos-1] = "en";
+        } else {
+        }
+        break;
+      case "ta": // tä
+        // TODO
+        if ($secondlast == "s" && $nos == 2) { // juosta, nousta
+          $w[$nos-2] = mb_substr($w[$nos-2], 0, -1)."k";
+          $w[$nos-1] = "sen";
+        } else if ($secondlast == "s") { // hotkaista
+          $w[$nos-1] = "en";
+        } else if ($secondlast == "i") { // ravita, suvaita, valita
+          $w[$nos-1] = "tsen";
+        } else if ($secondlast == "y" && isset($w[$nos-3]) && mb_strlen($w[$nos-3]) == 2) { // lymytä, rymytä, kärytä
+          $w[$nos-1] = "".$a."n";
+        } else if ($secondlast == "y") { // ryöpytä, röyhytä, löylytä
+          $w[$nos-1] = "t".$a."n";
+          
+//        if ($secondfirst == "a" || $secondfirst == "o" || $secondfirst == "u" || $secondfirst == "y"){ // diskota aallota > an
+//          $w[$nos-1] = $a."n";
+//        } else if ($secondlast == "i"){
+//          $w[$nos-1] = "t".$a;
+//        } else if($secondlast == "y"){
+//          $w[$nos-1] = "".$a;
+//        } else if($secondlast == "s"){
+          // remove s
+/*          $w[$this->nbr_of_sylls-2] = substr($w[$this->nbr_of_sylls-2],0,-1);
+          $w[$this->nbr_of_sylls-1] = "ksen";*/
+        } else {
+//          $w[$nos-1] = "en";
+        }
+        break;
+      case "a": // ä
+        if($thirdlast == "l" && $secondfirst == "p"){ // kylpeä
+          $w[$nos-2] = "v".mb_substr($w[$nos-2],1);
+        } else if ($thirdlast == "m" && $secondfirst == "p"){ // empiä
+          $w[$nos-2] = "m".mb_substr($w[$nos-2],1);
+
+        } else if ($thirdlast == "h" && $secondfirst == "t"){ // ahnehtia
+          $w[$nos-2] = "d".mb_substr($w[$nos-2],1);
+        } else if (in_array($thirdlast, $this->wovels) && $secondfirst == "t"){ // potea, kutea, päteä
+          $w[$nos-2] = "d".mb_substr($w[$nos-2],1);
+        } else if ($thirdlast == "t" && $secondfirst == "t"){ // asettua
+          $w[$nos-2] = mb_substr($w[$nos-2],1);
+        } else if ($thirdlast == "s" && $secondfirst == "t"){ // arvopaperistua, poistua
+          // stays the same
+          
+        } else if ($thirdlast == "n" && $secondfirst == "k"){ // henkiä
+          $w[$nos-2] = "g".mb_substr($w[$nos-2],1);
+        } else if ($thirdlast == "l" && $secondfirst == "k"){ // hylkiä
+          $w[$nos-2] = "j".mb_substr($w[$nos-2],1);
+        } else if (in_array($thirdlast, $this->wovels) && $secondfirst == "k"){ // hakea, kokea
+          $w[$nos-2] = mb_substr($w[$nos-2],1);
+          
+        } else if ($thirdlast == "p" && $secondfirst == "p"){ // harppia
+          $w[$nos-2] = mb_substr($w[$nos-2],1);
+        } else if (in_array($thirdlast, $this->wovels) && $secondfirst == "p"){ // hiipiä, kaapia, ruopia, ...
+          $w[$nos-2] = "v".mb_substr($w[$nos-2],1);
+        } else if ($this->sylls[$nos-2] == "tu"){
+          $w[$nos-2] = "du";
+        }
+        $w[$nos-1] = "n";
+        break;
+      case "paa": // lappaa, nappaa
+        $w[$nos-1] = $a."n";
+        break;
+      case "kaa": // alkaa, jakaa
+        if (in_array($secondlast, array("l", "a", "k"))) { // alkaa, jakaa, virkkaa
+          $w[$nos-1] = mb_substr($w[$nos-1],1,-1)."n";
+        } else { // jatkaa
+          $w[$nos-1] = mb_substr($w[$nos-1],0,-1)."n";
+        }
+        break;
+      case "jaa": // ajaa
+      case "nee": // tarkenee
+      case "laa": // palaa
+      default: // jaksaa, maksaa, jauhaa, kalvaa, nauraa, painaa
+        $w[$nos-1] = mb_substr($w[$nos-1],0,-1)."n";
+        break;
+    }
+)
+    
+    return $this->buildWord($w);
+  }
+  
+  private function buildWord($w){
+    return implode('', $w);
+  }
+  
+  
+  /**
+  * return array
+  */
+  private function syllabs($word){
+    if(empty($word)){
+      return array();
+    }
+    $this->orig = array();
+    $orig = $word;
+    
+    $cons = array("b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "z");
+    $wovels = array("a", "e", "i", "o", "u", "y");
+    // diftongit
+    $dif = array("yi", "ui", "oi", "ai", "ay", " au", "yo", "oy", "uo", "ou", "ie", "ei", "eu", "iu", "ey", "iy");
+
+    $word = trim($word);
+    // utf stuff. a:ksi ja o:ksi
+    $word = str_replace(array("å", "ä", "ö", "Å", "Ä", "Ö"), array("a", "a", "o", "a", "a", "o"), $word);
+    $loop = mb_strlen($word);
+    
+    // split the word
+    $w = str_split(mb_strtolower($word));
+    $w[] = " "; // helpers so we dont get notice's
+    $w[] = " ";
+    $w[] = " ";
+    $w[] = " ";
+
+    // put the word here letters and -'s
+    $com_word = array();
+    for($i = 0;$i < $loop;){
+      $d = 1; // how many digits forward
+      if(in_array($w[$i], $cons)){
+        if(($i+1) >= $loop){ // if last is kons, remove the possible previous -
+          $last = array_pop($com_word);
+          if($last != "-"){
+            $com_word[] = $last;
+          }
+        }
+        $com_word[] = $w[$i];
+      } else if(in_array($w[$i], $wovels)){
+        $com_word[] = $w[$i];
+        if(in_array($w[$i].$w[$i+1], $dif) || $w[$i] == $w[$i+1] || $w[$i+1] == "i"){ // next diftongi, same vokaali or "i" 
+          $com_word[] = $w[$i+1];
+          $d = 2;
+          if(in_array($w[$i+2], $wovels)){
+            $com_word[] = "-";
+          } else if(in_array($w[$i+2], $cons) && in_array($w[$i+3], $cons) && in_array($w[$i+4], $cons)){
+            $com_word[] = $w[$i+2];
+            $com_word[] = $w[$i+3];
+            $com_word[] = "-";
+            $d = 4;
+          } else if(in_array($w[$i+2], $cons) && in_array($w[$i+3], $cons)){
+            $com_word[] = $w[$i+2];
+            $com_word[] = "-";
+            $d = 3;
+          } else if(in_array($w[$i+2], $cons)){
+            $com_word[] = "-";
+            $d = 2;
+          }
+        } else if(in_array($w[$i+1], $wovels)){
+          $com_word[] = "-";
+          $d = 1;
+        } else {
+          if(in_array($w[$i+1], $cons) && in_array($w[$i+2], $cons) && in_array($w[$i+3], $cons)){
+            $com_word[] = $w[$i+1];
+            $com_word[] = $w[$i+2];
+            $com_word[] = "-";
+            $d = 3;
+          } else if(in_array($w[$i+1], $cons) && in_array($w[$i+2], $cons)){
+            $com_word[] = $w[$i+1];
+            $com_word[] = "-";
+            $d = 2;
+          } else if(in_array($w[$i+1], $cons)){
+            $com_word[] = "-";
+            $d = 1;
+          }
+        }
+      }
+      $i = $i + $d;
+    }
+    // now build the word back together
+    $sylls = array();
+    $tindex = 0;
+    $windex = 0; // word index
+    foreach($com_word AS $in => $letter){
+      if($letter == "-"){
+        $tindex++;
+      } else if(isset($sylls[$tindex])){
+        $sylls[$tindex] .= $letter;
+        $this->orig[$tindex] .= mb_substr($orig, $windex, 1);
+        $windex++;
+      } else {
+        $sylls[$tindex] = $letter;
+        $this->orig[$tindex] = mb_substr($orig, $windex, 1);
+        $windex++;
+      }
+    }
+    $this->last_syllabus = $sylls[$tindex];
+    $this->nbr_of_sylls = $tindex + 1;
+    $this->sylls = $sylls;
+  }
+  
+  
+  /**
+  * check if the word is back wovel word
+  * NOTE: HAS TO BE CALLED BEFORE conjugateWord function
+  * @param string $word string to check
+  * @return boolean is back wovel
+  */
+  private function isBackWovelWord($word) {
+    // if the same word is being checked, use the previous result instead of matching again
+    if ($word == $this->word && $this->backVowelWord !== null) {
+      return $this->backVowelWord;
+    } else {
+      // nullify
+      $this->backVowelWord = null;
+    }
+    
+    $backVowelPos = -1;
+    $apos = mb_strrpos($word, "a");
+    if ($apos !== false) {
+      $backVowelPos = $apos;
+    }
+    $opos = mb_strrpos($word, "o");
+    if ($opos !== false && $opos > $backVowelPos) {
+      $backVowelPos = $opos;
+    }
+    $upos = mb_strrpos($word, "u");
+    if ($upos !== false && $upos > $backVowelPos) {
+      $backVowelPos = $upos;
+    }
+    
+    $frontVowelPos = -1;
+    $apos = mb_strrpos($word, "ä");
+    if ($apos !== false) {
+      $frontVowelPos = $apos;
+    }
+    $opos = mb_strrpos($word, "ö");
+    if ($opos !== false && $opos > $frontVowelPos) {
+      $frontVowelPos = $opos;
+    }
+    $upos = mb_strrpos($word, "y");
+    if ($upos !== false && $upos > $frontVowelPos) {
+      $frontVowelPos = $upos;
+    }
+    
+    if ($frontVowelPos == -1 && $backVowelPos == -1) { // only i's and e's
+      $this->backVowelWord = false;
+    } else if ($backVowelPos >= 0 && $frontVowelPos == -1) { // there is a, o or u and no ä, ö or y
+      $this->backVowelWord = true;
+    } else if ($backVowelPos == -1 && $frontVowelPos >= 0) { // there is  ä, ö or y and no a, o or u
+      $this->backVowelWord = false;
+    } else if ($backVowelPos > $frontVowelPos) { // both present (combined word)
+      $this->backVowelWord = true;
+    } else if ($backVowelPos < $frontVowelPos){  // both present frontWovel later
+      $this->backVowelWord = false;
+    }
+    return $this->backVowelWord;
+  }
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// OLD STUFF BELOW .... REMOVE WHEN ABOVE IS DONE
+
+
 /**
 * class to handle all
 */
